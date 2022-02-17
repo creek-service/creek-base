00b70edec157509a1735b87a6b39f56292c44de2
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-02-17 13:36:03 +0000

    Initial customisation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
-rootProject.name = "multi-module-template" // ChangeMe: set the root project name with the module name.
+rootProject.name = "creek-base"
 
 include(
-    "example"
+    "types",
+    "annotations"
 )
