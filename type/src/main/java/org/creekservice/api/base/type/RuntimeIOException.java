4daa04c35ebb881bb1f7ca2d717ab13644b61c85
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2022-06-23 12:13:56 +0100

    Add `RuntimeIOException` type (#68)

---
+++ type/src/main/java/org/creekservice/api/base/type/RuntimeIOException.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 Creek Contributors (https://github.com/creek-service)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.creekservice.api.base.type;
+
+
+import java.io.IOException;
+
+/** As checked exceptions are a PITA. */
+public final class RuntimeIOException extends RuntimeException {
+
+    public static RuntimeIOException runtimeIOException(final IOException cause) {
+        return new RuntimeIOException(cause);
+    }
+
+    public static RuntimeIOException runtimeIOException(final String msg, final IOException cause) {
+        return new RuntimeIOException(msg, cause);
+    }
+
+    private RuntimeIOException(final IOException cause) {
+        super(cause);
+    }
+
+    private RuntimeIOException(final String msg, final IOException cause) {
+        super(msg, cause);
+    }
+}
