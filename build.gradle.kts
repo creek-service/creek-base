890f4a245dae696ea56966be5c3d6d662c1fe3d7
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2023-01-30 23:26:36 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ subprojects {
     }
 
     extra.apply {
-        set("creekVersion", "0.3.1")
+        set("creekVersion", "0.3.2-SNAPSHOT")
 
         set("spotBugsVersion", "4.7.3")         // https://mvnrepository.com/artifact/com.github.spotbugs/spotbugs-annotations
         set("classGraphVersion", "4.8.154")     // https://mvnrepository.com/artifact/io.github.classgraph/classgraph
